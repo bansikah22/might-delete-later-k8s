d24e5ba5c90dc5325a3245761d675f4bb5dad778
nta <NoelBansikah.Tandap@adorsys.com>
2026-01-28 15:13:31 +0100

    feat: hashcode , equals and hashset

--- java/records/Main.java
+++ java/records/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 	public static void main(String[] args) {
 
@@ -9,6 +12,33 @@ public class Main {
         System.out.println("User Age: " + u1.age());
         System.out.println("UserClass Name: " + uc1.getName());
         System.out.println("UserClass Age: " + uc1.getAge());
+
+        //UserClass uc2 = new UserClass("Eve", -10);
+        //System.out.println(uc2.getAge());
+
+        //print everything
+        System.out.println(u1);
+        System.out.println(uc1);
+
+
         System.out.println("Hello, Records!");
+
+        //equals and hashcode
+        User u2 = new User("Alice", 30);
+        System.out.println("u1 == u2: " + (u1 == u2)); // == checks reference equality not data equality so we have false
+        System.out.println("u1.equals(u2): " + u1.equals(u2)); // equals checks data equality so we have true
+
+        System.out.println("u1.hashCode(): " + u1.hashCode());
+        System.out.println("u2.hashCode(): " + u2.hashCode());
+
+        System.out.println("Same hashcode: " + (u1.hashCode() == u2.hashCode()));
+
+        Set<User> set = new HashSet<>();
+        set.add(u1);
+        set.add(u2); // should not add duplicate
+        System.out.println("Set size: " + set.size()); // should be 1
+        
+
+
 	}
 }
--- java/records/UserClass.java
+++ java/records/UserClass.java
@@ -3,9 +3,13 @@ public class UserClass {
     private int age;
 
     public UserClass(String name, int age) {
+      if (age <= 0) {
+        throw new IllegalArgumentException("Age cannot be negative or zero");
+      }
         this.name = name;
         this.age = age;
     }
+
 
     public String getName() {
         return name;
