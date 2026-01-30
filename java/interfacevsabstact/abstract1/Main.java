3290540e378f52a80a80b9bf9790e0dd2d33e49c
nta <NoelBansikah.Tandap@adorsys.com>
2026-01-30 13:37:32 +0100

    chore: add notes on abstract

--- java/interfacevsabstact/abstract1/Main.java
+++ java/interfacevsabstact/abstract1/Main.java
@@ -1,4 +1,16 @@
 package java.interfacevsabstact.abstract1;
+/**
+ * Demonstrates the use of abstract classes in Java.
+ * Here, we define an abstract class Shape with an abstract method area().
+ * The Circle class extends Shape and provides an implementation for the area() method.
+ * In the Main class, we create an instance of Circle and call its methods.
+ * Abstract classes allow us to define methods that must be implemented by subclasses,
+ * providing a common interface while allowing for specific implementations.
+ * @author Noel Bansikah
+ * @date 29 01 2026
+ * 
+ */
+
 
 public class Main {
     public static void main(String[] args) {
